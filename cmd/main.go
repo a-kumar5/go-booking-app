eea7f3586954deb7f239e6cfaf3f73bf33860cb1
a-kumar5 <ayush.1306.kumar@gmail.com>
2024-05-08 12:10:30 +0530

    WIP: Slice and array practice

--- cmd/main.go
+++ cmd/main.go
@@ -8,4 +8,8 @@ import (
 
 func main() {
 	fmt.Println(hello.Say(os.Args[1:]))
+	var w = [...]int{1, 2, 3}
+	var x = []int{0, 0, 0}
+	y := hello.Do(w, x)
+	fmt.Println(w, x, y)
 }
--- hello.go
+++ hello.go
@@ -10,3 +10,14 @@ func Say(names []string) string {
 	}
 	return "Hello, " + strings.Join(names, ", ") + "!"
 }
+
+func Do(a [3]int, b []int) []int {
+	// a = b //syntax error
+	a[0] = 4
+	b[0] = 3
+
+	c := make([]int, 5)
+	c[4] = 42
+	copy(c, b)
+	return c
+}
