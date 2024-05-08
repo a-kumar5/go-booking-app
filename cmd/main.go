9203c3b38eaf69daa747ee71e303a345d5c41372
akumar5 <ayush.kumar@transformco.com>
2024-05-08 17:11:58 +0530

    Sorting example added

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"hello"
-	"os"
+	//"os"
 )
 
 func main() {
-	fmt.Println(hello.Say(os.Args[1:]))
-	var w = [...]int{1, 2, 3}
-	var x = []int{0, 0, 0}
-	y := hello.Do(w, x)
-	fmt.Println(w, x, y)
+	//fmt.Println(hello.Say(os.Args[1:]))
+	//var w = [...]int{1, 2, 3}
+	//var x = []int{0, 0, 0}
+	//y := hello.Do(w, x)
+	//fmt.Println(w, x, y)
+	// sorting - map examples
+
+	hello.Sort()
 }
--- hello.go
+++ hello.go
@@ -2,6 +2,10 @@ package hello
 
 import (
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
+	"sort"
 )
 
 func Say(names []string) string {
@@ -20,4 +24,37 @@ func Do(a [3]int, b []int) []int {
 	c[4] = 42
 	copy(c, b)
 	return c
+}
+
+func Sort(){
+	scan := bufio.NewScanner(os.Stdin)
+	words := make(map[string]int)
+	scan.Split(bufio.ScanWords)
+	for scan.Scan(){
+		words[scan.Text()]++
+	}
+	fmt.Println(len(words), "unique words")
+
+	type kv struct {
+		key string
+		val int
+	}
+
+	var ss []kv
+
+	for k, v := range words {
+		ss = append(ss, kv{k, v})
+	}
+
+	fmt.Println(ss)
+
+	sort.Slice(ss, func(i, j int) bool{
+		return ss[i].val > ss[j].val
+	})
+	fmt.Println(ss)
+
+	for _, s := range ss {
+		fmt.Println(s.key, "appears", s.val, "times")
+	}
+
 }
