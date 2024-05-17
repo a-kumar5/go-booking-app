9903336ce344691ce9a0246be77ab7ce6a00b0f0
akumar5 <ayush.kumar@transformco.com>
2024-05-17 19:37:09 +0530

    WIP : the go programming language problem

---
+++ serverhttp/homework/downloader.go
@@ -0,0 +1,14 @@
+/*
+The first program downloads comic metadata
+   1. We will read until we get two 404 responses in a row
+   2. Each request will generate a JSON object as a string
+   3. We will bracket them with [ and ] and a comma between
+   4. The result will be a file with a JSON array of metadata objects, so we won't need to decode
+   5. We will optionally take a filename from the command line for output
+*/
+
+package main
+
+func main() {
+
+}
